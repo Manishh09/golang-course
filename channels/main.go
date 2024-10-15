c6542b65969e9fafb85208df9f7ace8bc1b0d7c9
Manish Boge (SX/EDC2) <manish.boge@in.bosch.com>
2024-10-15 13:39:48 +0530

    chore: Refactor main.go to improve readability and add sleep to go routines

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -17,7 +18,7 @@ func main() {
 	ch := make(chan string) // creating a channel of type string
 
 	for _, link := range links {
-		go checkStatus(link, ch) // pass channel
+		go checkRepeatGoRoutines(link, ch) // pass channel
 	}
 	// Main GORoutine will wait till the channel receives the value, so its a blocking line of code // waits for the resp from child goroutines
 
@@ -29,9 +30,40 @@ func main() {
 
 	//we need wait for all the goroutines,  since 5 go routines were created
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-ch) // wait for channel to receive data and resumes the loop execution
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-ch) // wait for channel to receive data and resumes the loop execution
+	// }
+
+	// check repeated go routines to check the status of each
+	// this for-loop runs continuously
+	// for {
+	// 	go checkRepeatGoRoutines(<-ch, ch) // wait for channel to receive data and resumes the loop execution
+	// }
+
+	// wait for ch till has its value
+	// run body of the for loop
+	// alternative to above for-loop
+	// more concise
+
+	/*
+		for l := range ch {
+
+			go checkRepeatGoRoutines(l, ch) // wait for channel to receive data and resumes the loop execution
+
+		}
+	*/
+
+	// add sleep to each go routines
+
+	for l := range ch {
+
+		go func(link string) { // function literal syntax
+			time.Sleep(5 * time.Second)
+			checkRepeatGoRoutines(link, ch) // wait for channel to receive data and resumes the loop execution
+		}(l)
+
 	}
+
 }
 
 func checkStatus(link string, ch chan string) {
@@ -49,3 +81,19 @@ func checkStatus(link string, ch chan string) {
 }
 
 // Note: Receiving the data into a channel referred as a Blocking Call
+
+func checkRepeatGoRoutines(link string, ch chan string) {
+	// time.Sleep(5 * time.Second) ;// not recommended
+
+	_, err := http.Get(link) // blocking call - code takes lil time to execute --> Main GoRoutine stops here
+
+	if err != nil {
+		fmt.Println(link, "could be down!")
+		ch <- link // keep message in channel
+		return
+	}
+
+	fmt.Println(link, "is up !!")
+
+	ch <- link //Child GoRoutines gets the value from server and  keep message in channel // once this gets executed, then the  MainGORoutine will be notified that something is inside the channel that is in the MainGORoutine , so in this way, MaiGoRoutine will not exit the program
+}
